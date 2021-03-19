3b40e7a1a46f868debec2a491dfad6d6bbc854a2
이경창 <lkc263@naver.com>
2021-03-19 15:35:44 +0900

    study python, day 3

---
+++ Python/code/Day 2/testlist.py
@@ -0,0 +1,13 @@
+drawer = ["양말", "속옷", "모자", "반팔", "바지"]
+
+del drawer[3]
+print(drawer)
+
+drawer[3] = ""
+print(drawer)
+
+drawer[2] = "점퍼"
+print(drawer)
+
+drawer[2]
+print(drawer)
---
+++ Python/code/Day 3/dictionary.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+phones = {"model":"iphoneXS","manufacturer":"apple",
+"year":"2018"}
+
+print(phones)
+
+del phones["year"]
+print(phones)
